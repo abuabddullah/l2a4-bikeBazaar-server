52dfc10103b78394edcd247ee79cc03f86bbbf8d
asifaowadud <asifaowadud@gmail.com>
2025-02-15 18:58:34 +0600

    backend ready

--- dist/middlewares/upload.js
+++ dist/middlewares/upload.js
@@ -7,16 +7,11 @@ exports.upload = void 0;
 const multer_1 = __importDefault(require("multer"));
 const multer_storage_cloudinary_1 = require("multer-storage-cloudinary");
 const cloudinary_1 = __importDefault(require("./../utils/cloudinary"));
-// cloudinary.config({
-//   cloud_name: config.CLOUDINARY_CLOUD_NAME,
-//   api_key: config.CLOUDINARY_API_KEY,
-//   api_secret: config.CLOUDINARY_API_SECRET,
-// });
 const storage = new multer_storage_cloudinary_1.CloudinaryStorage({
     cloudinary: cloudinary_1.default,
     params: async (req, file) => ({
         folder: "bicycle-shop",
-        format: file.mimetype.split("/")[1], // Extract format dynamically
+        format: file.mimetype.split("/")[1],
         public_id: `${file.fieldname}-${Date.now()}`,
         transformation: [{ width: 500, height: 500, crop: "limit" }],
     }),
